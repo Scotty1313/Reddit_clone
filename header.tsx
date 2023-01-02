a29da249f70d0eb4f55fc0385143add41b6068fd
Scotty1313 <63591088+Scotty1313@users.noreply.github.com>
2023-01-02 22:10:20 +0100

    Add files via upload
    Added Sign In/Out option using Next Auth and Reddits API

--- header.tsx
+++ header.tsx
@@ -16,8 +16,12 @@ import {
     SpeakerphoneIcon,
     VideoCameraIcon,
 } from "@heroicons/react/outline"
+import { signIn, signOut, useSession } from "next-auth/react"
+
 
 export default function Header() {
+    const { data: session } = useSession()
+
     return (
         <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
             <div className="relative h-10 w-40 flex-shrink-0 cursor-pointer">
@@ -66,12 +70,33 @@ export default function Header() {
             </div>
 
             {/* Sign in/out button */}
-            <div className="hidden cursor-pointer items-center space-x-2 border
-            border-gray-100 p-2 lg:flex">
-                <div className="relative h-5 w-5 flex-shrink-0">
-                    <UserIcon />
+            { session ? (
+                <div onClick={() => signOut()} 
+                className="hidden cursor-pointer items-center space-x-2 border
+                 border-gray-100 p-2 lg:flex">
+                    <div className="relative h-5 w-5 flex-shrink-0">
+                        <UserIcon />
+                    </div>
+
+                    <p className="text-gray-400">Sign Out</p>
                 </div>
-            </div>
+            ) : (
+                <div>
+                    <div onClick={() => signIn()} 
+                    className="hidden cursor-pointer items-center space-x-2 border
+                     border-gray-100 p-2 lg:flex">
+                        <div className="relative h-5 w-5 flex-shrink-0">
+                            <UserIcon />
+                        </div>
+                    </div>
+
+                    <p className="text-gray-400">Sign In</p>
+                </div>
+        
+            )
+            }
+
+            
         </div>
     )
 }
