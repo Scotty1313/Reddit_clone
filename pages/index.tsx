964819d7eadb9a95120e0d32c8e72449e3b07fad
Scotty1313 <63591088+Scotty1313@users.noreply.github.com>
2023-01-04 22:56:18 +0100

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,23 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Header from '../components/header'
+import type { NextPage } from "next"
+import Head from "next/head"
+import PostBox from "../components/PostBox"
 
-export default function Home() {
+
+const Home: NextPage = () => {
   return (
-    <>
+    <div>
       <Head>
         <title>Reddit clone by Scott Macdonald</title>
         <meta name="description" content="Reddit clone by Scott Macdonald" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main>
-        <div>
-          <h1></h1>
-        </div>
-      </main>
 
-      <Header />
-    </>
+      <PostBox />
+      <div>
+
+      </div>
+    </div>
   )
 }
+
+export default Home
