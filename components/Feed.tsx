df74fc5b41399453f2e875733e0cd1ecf5607f26
Scotty1313 <63591088+Scotty1313@users.noreply.github.com>
2023-01-23 16:03:34 +0100

    Add files via upload

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,4 +1,11 @@
+import { useQuery } from "@apollo/client"
+import { GET_ALL_POSTS } from "../graphql/queries"
+
 export default function Feed() {
+    const { data, error } = useQuery(GET_ALL_POSTS)
+
+    const posts: Post[] = data?.getPostList;
+
     return (
         <div>Feede goes here</div>
     )
