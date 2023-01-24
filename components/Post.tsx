501da41023ea54566289dde7100460859f0d6050
Scotty1313 <63591088+Scotty1313@users.noreply.github.com>
2023-01-24 16:41:40 +0100

    Add files via upload

---
+++ components/Post.tsx
@@ -0,0 +1,47 @@
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  BookmarkIcon,
+  ChatAltIcon,
+  DotsHorizontalIcon,
+  GiftIcon,
+  ShareIcon,
+} from "@heroicons/react/outline"
+import Avatar from "./Avatar"
+
+
+type Props = {
+    post: Post
+}
+
+export default function Post({ post }: Props) {
+  return (
+  <div className="rounded-md flex cursor-pointer border border-gray-300
+  bg-white shadow-sm hover:border-gray-600">
+    {/*Votes*/}
+    <div className="flex flex-col items-center justify-start space-y-1
+    rounded-l-md bg-gray-50 p-4 text-gray-400">
+      <ArrowUpIcon className="voteButtons hover:text-red-400"/>
+      <p className="text-black text-xs font-bold">0</p>
+      <ArrowDownIcon className="voteButtons hover:text-blue-400"/>
+    </div>
+
+  <div className="p-3 pb-1">
+    {/* Header */}
+    <div>
+      <Avatar seed={post.subreddit[0]?.topic} />
+      <p>
+        <span>r/{post.subreddit[0]?.topic}</span>
+      </p>
+    </div>
+    {/* Body */}
+
+    {/* Image */}
+
+    {/* Footer */}
+  </div>
+</div>
+  )
+}
+
+ 
