c7d2fed51ede47934ccf3d8aa9cc9ba67c4c373e
Scotty1313 <63591088+Scotty1313@users.noreply.github.com>
2023-02-01 18:13:20 +0100

    Add files via upload
    fixed mutation issue

--- graphql/mutations.ts
+++ graphql/mutations.ts
@@ -24,14 +24,13 @@ mutation MyMutation(
         username
     }
 }`
-{/* check mutation at 2:12 ish
+
 
 export const ADD_SUBREDDIT = gql`
-mutation MyMutation(topic: String!) {
+mutation MyMutation($topic: String!) {
     insertSubreddit(topic: $topic) {
         id
         topic
         created_at
     }
 }`
-*/}
