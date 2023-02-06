16814b5eb9fff207d3f8f0d2cce505180f3b42c6
Scotty1313 <63591088+Scotty1313@users.noreply.github.com>
2023-02-06 16:57:07 +0100

    Add files via upload

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,3 +1,6 @@
+import TimeAgo from 'timeago-react'
+import Avatar from "./Avatar"
+
 import {
   ArrowDownIcon,
   ArrowUpIcon,
@@ -7,7 +10,7 @@ import {
   GiftIcon,
   ShareIcon,
 } from "@heroicons/react/outline"
-import Avatar from "./Avatar"
+
 
 
 type Props = {
@@ -17,7 +20,7 @@ type Props = {
 export default function Post({ post }: Props) {
   return (
   <div className="rounded-md flex cursor-pointer border border-gray-300
-  bg-white shadow-sm hover:border-gray-600">
+  bg-white shadow-sm hover:border hover:border-gray-600">
     {/*Votes*/}
     <div className="flex flex-col items-center justify-start space-y-1
     rounded-l-md bg-gray-50 p-4 text-gray-400">
@@ -28,13 +31,23 @@ export default function Post({ post }: Props) {
 
     <div className="p-3 pb-1">
       {/* Header */}
-      <div>
-        <Avatar seed={post.subreddit[0]?.topic}  />
-        <p>
-          <span>r/{post.subreddit[0]?.topic}</span>
+      <div className="flex items-center space-x-2">
+      <Avatar seed={post.subreddit[0]?.topic}  />
+        <p className="text-xs text-gray-400">
+          <span className="font-bold text-black hover:text-bluee-400 
+          hover:underline">
+            r/{post.subreddit[0]?.topic}
+            </span>
+             • Posted by u/{post.username}
+          <TimeAgo datetime={post.created_at} />
         </p>
       </div>   
+
       {/* Body */}
+      <div className="py-4">
+        <h2 className="text-xl font-semibold">{post.title}</h2>
+        <p className="mt-2 text-xs font-light">{post.body}</p>
+      </div>
 
       {/* Image */}
 
