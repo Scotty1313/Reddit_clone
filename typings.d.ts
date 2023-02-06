01e4490aeb7574e1bd00d807f282fbc7e68df9cd
Scotty1313 <63591088+Scotty1313@users.noreply.github.com>
2023-02-06 16:59:45 +0100

    Add files via upload

--- typings.d.ts
+++ typings.d.ts
@@ -1,34 +1,34 @@
-type Comment = {
-    created_at: DateTime
-    id: ID!
-    post_id: ID
-    text: String
-    username: String
+type Comments = {
+    created_at: string
+    id: number
+    post_id: number
+    text: string
+    username: string
   }
 
   type Vote = {
-    created_at: DateTime
-    id: ID!
-    post_id: ID
-    upvote: Boolean
-    username: String
+    created_at: string
+    id: number
+    post_id: number
+    upvote: boolean
+    username: string
   }
 
   type Subreddit = {
-    created_at: DateTime
-    id: ID!
-    topic: String
+    created_at: string
+    id: number
+    topic: string
   }
 
   type Post = {
-    body: String
-    created_at: DateTime
-    id: ID!
-    image: String
-    subreddit: Subreddit[]
-    subreddit_id: ID
-    title: String
-    username: String
+    body: string
+    created_at: string
+    id: number
+    image: string
+    subreddit_id: number
+    title: string
+    username: string
     votes: Vote[]
     comments: Comments[]
+    subreddit: Subreddit[]
   }
