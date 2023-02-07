98375ed1e5fafb1b1a893ba0f0fc9596e58265c7
Scotty1313 <63591088+Scotty1313@users.noreply.github.com>
2023-02-07 21:40:02 +0100

    Add files via upload

--- components/Post.tsx
+++ components/Post.tsx
@@ -46,12 +46,34 @@ export default function Post({ post }: Props) {
       {/* Body */}
       <div className="py-4">
         <h2 className="text-xl font-semibold">{post.title}</h2>
-        <p className="mt-2 text-xs font-light">{post.body}</p>
+        <p className="mt-2 text-sm font-light">{post.body}</p>
       </div>
 
       {/* Image */}
+      <img className="w-full" src={post.image} alt="post_image" />
 
       {/* Footer */}
+      <div className="flex space-x-4 text-gray-400">
+        <div className="postButtons">
+          <ChatAltIcon className="h-6 w-6" />
+          <p className="">{post.comments.length} Comments</p>
+        </div>
+        <div className="postButtons">
+          <GiftIcon className="h-6 w-6" />
+          <p className="hidden sm:inline">Give Award</p>
+        </div>
+        <div className="postButtons">
+          <ShareIcon className="h-6 w-6" />
+          <p className="hidden sm:inline">Share</p>
+        </div>
+        <div className="postButtons">
+          <BookmarkIcon className="h-6 w-6" />
+          <p className="hidden sm:inline">Save</p>
+        </div>
+        <div className="postButtons">
+          <DotsHorizontalIcon className="h-6 w-6" />
+        </div>
+      </div>
     </div>
   </div>
   )
